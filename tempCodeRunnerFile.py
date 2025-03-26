2e8b43ee6c59bb76187a9d9b5b23de3fc4082824
Sparsh2509 <sparshgupta250904@gmail.com>
2025-03-26 17:23:45 +0530

    Decision tree classifier preprunning algo started

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-y_pred=treemodel.predict(X_test)
-print(y_pred)
-
-from sklearn.metrics import accuracy_score,classification_report
-score=accuracy_score(y_pred,y_test)
-print(score)
-
-print(classification_report(y_pred,y_test))
